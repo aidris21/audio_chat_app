203e6d7c14d4a6f2eacabe0a9759b2854b75eedc
Amir Idris <amiridris@amirs-mbp.mynetworksettings.com>
2024-01-20 17:06:55 -0500

    gpt

--- whisper_booth/chat_app/views.py
+++ whisper_booth/chat_app/views.py
@@ -4,7 +4,11 @@ from django.template import loader
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
-from .models import Message
+from .models import Message, AITranscript
+
+from ai_chat.chat_with_gpt import get_gpt_response_from_prompt
+
+DEFAULT_AI_USERNAME = "HAL9000"
 
 def index(request):
     if not request.user.is_authenticated:
@@ -15,22 +19,40 @@ def index(request):
 def messages(request):
     if not request.user.is_authenticated:
         return redirect("login")
+    
     last_10_messages = Message.objects.filter(by_user=request.user).order_by("at_time")[:10]
+    last_10_ai_messages = AITranscript.objects.filter(to_user=request.user).order_by("at_time")[:10]
+    messages = []
+    for message in last_10_messages:
+        messages.append(
+            {"text": message.text, "at_time": message.at_time, "by_user": message.by_user.username}
+        )
+    for message in last_10_ai_messages:
+        messages.append(
+            {"text": message.text, "at_time": message.at_time, "by_user": DEFAULT_AI_USERNAME}
+        )
+        
+    messages.sort(reverse=True, key=lambda message: message["at_time"])
+
     context = {
-        "messages": last_10_messages,
+        "messages": messages,
     }
     
     return render(request, "chat_app/messages.html", context)
 
 
 def send_message(request):
-    if request.method == "POST":
-        print(request.POST.keys())
-        text = request.POST.get('chat-msg', None)
-        if text is not None and text != '':
-            new_message = Message(text=text, by_user=request.user, at_time=timezone.now())
-            new_message.save()
-        return redirect('chat_app:messages')
-    else:
+    if request.method != "POST":
         return HttpResponse('Request should be POST.')
+
+    text = request.POST.get('chat-msg', None)
+    if text is not None and text != '':
+        new_message = Message(text=text, by_user=request.user, at_time=timezone.now())
+        new_message.save()
+
+        gpt_response = get_gpt_response_from_prompt(text)
+        new_transcript = AITranscript(text=gpt_response, to_user=request.user, at_time=timezone.now())
+        new_transcript.save()
+
+    return redirect('chat_app:messages')
 
