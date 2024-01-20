42da5d2718c73bcf7fda94710a12b498abed21bb
Amir Idris <amiridris@amirs-mbp.mynetworksettings.com>
2024-01-20 12:55:36 -0500

    admin and string methods

--- whisper_booth/chat_app/models.py
+++ whisper_booth/chat_app/models.py
@@ -6,8 +6,14 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Message(models.Model):
     text = models.TextField(max_length=500)
     by_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     at_time = models.DateTimeField("message time")
+
+    def __str__(self):
+        return f"{self.text[0:50]}..."
