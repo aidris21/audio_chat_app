ec1a6f0925f4f46500a993191c629afd30af153f
Amir Idris <amiridris@amirs-mbp.mynetworksettings.com>
2024-01-20 14:45:37 -0500

    update message

--- whisper_booth/chat_app/models.py
+++ whisper_booth/chat_app/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     text = models.TextField(max_length=500)
-    by_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    by_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='by_user')
+    to_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='to_user')
     at_time = models.DateTimeField("message time")
 
     def __str__(self):
--- whisper_booth/whisper_booth/settings.py
+++ whisper_booth/whisper_booth/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "accounts.apps.AccountsConfig",
     "chat_app.apps.ChatAppConfig",
     "django.contrib.admin",
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "channels",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/chat_app"
+
+ASGI_APPLICATION = 'whisper_booth.asgi.application'
