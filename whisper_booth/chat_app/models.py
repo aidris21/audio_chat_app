c5964230d3d2a5c4d97dc6b031ebc53aa9b5fe46
Amir Idris <amiridris@amirs-mbp.mynetworksettings.com>
2024-01-20 12:38:26 -0500

    migrate models

--- whisper_booth/chat_app/models.py
+++ whisper_booth/chat_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class Message(models.Model):
+    text = models.TextField(max_length=500)
+    by_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    at_time = models.DateTimeField("message time")
